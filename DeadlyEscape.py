77eb5f92b8e7a777a1eff30170075122cc564e59
John Thomson <johndavthomson@proton.me>
2024-07-18 17:07:25 +0100

    Added second choice

--- DeadlyEscape.py
+++ DeadlyEscape.py
@@ -17,9 +17,9 @@ if choice1 == "door":
     time.sleep(3)
     print("You pull firmly but the door is heavy and sticky like there is a force pulling on the other side. The door opens slowly with a loud and rusty creeking noise.")
     time.sleep(3)
-    print("As the door opens, it reveales a long dim lighted hallway.")   
+    print("As the door opens, it reveales a long dim lighted hallway.")  
 else:
-    print("You head over to the window, it is locked and at closer inspection the glass is fire glass and therefore unbreakable.")
+    print("You head over to the window, it is locked and at closer inspection the glass is fireproof glass and therefore unbreakable.")
     time.sleep(3)
     print("Outside of the window is pure blackness, no sky, no trees, no buildings and no lights of any kind.")
     time.sleep(3)
@@ -44,20 +44,63 @@ print("You turn to run back into the room but the heavy door slams closed behind
 time.sleep(3)
 print("Everything is silent for a few moments, then you hear what sounds like gears moving in the walls. You place your ear against the wall to hear better but when you do, the gears stop for a few seconds then there is a very loud mechanical CLICK! ")
 time.sleep(3)
-print("The lights that are dimmly lighting the hallway start to turn off systematiclly. As you look down the hallway in both direction's, the lights in the distance turn off, placing the corridor in blackness and that blackness is moving towards you.")
+print("The lights that are dimmly lighting the hallway start to turn off systematiclly. As you look down the hallway in both direction's, the lights in the distance turn off, placing the corridor in darkness and that darkness is moving towards you.")
 time.sleep(3)
 print("With each light that goes out as it moves towards you, gets faster and faster")
 
-choice2 = input('What do you do ??, type "left" to run down the left corridor. Type "right" to run down the right corridor.')
+choice2 = input('What do you do ??, type "left" to run down the left corridor. Type "right" to run down the right corridor.' \n).lower()
 
 if choice2 == "left":
-   print("As your adrenaline kicks in, you sprint off down the corridor, struggling to breath.")
-   time.sleep(3)
-   print("As you run, all sounds fall into complete silence which causes you to stop running and listen intently. After a few seconds you begin to hear whispers, growling, snarling and howling.")
-   time.sleep(3)
-   print("You start to run again, stubbling over your own feet. Your crying and struggling to breath. You see a dimly light green sign hanging from the ceiling about 100 feet in front of you. The sign reads 'FIRE EXIT'.")
-   time.sleep(3)
-   print("As you come to the end of the corridor, everything is in complete darkness and the only light is from the sign above you")
-else:
-    print("Game Over!")
+    print("As your adrenaline kicks in, you sprint off down the corridor, struggling to breath.")
+    time.sleep(3)
+    print("As you run, all sounds fall into complete silence which causes you to stop running and listen intently. After a few seconds you begin to hear whispers, growling, snarling and howling.")
+    time.sleep(3)
+    print("You start to run again, stubbling over your own feet. Your crying and struggling to breath. You see a dimly light green sign hanging from the ceiling about 100 feet in front of you. The sign reads 'FIRE EXIT'.")
+    time.sleep(3)
+    print("As you come to the end of the corridor, everything is in complete darkness and the only light is from the sign above you")
     
+else:
+    print("As the darkness approaches you, your frozen in place with fear. As the last light goes out and you stand in complete darkness for a few seconds then the emergency lights kick in illuminating the corridor in a dim misty red hue.")
+    time.sleep(3)
+    print("you hear breathing right behind you, which makes your heartbeat faster than you have ever felt it beat in your life.")
+    time.sleep(3)
+    print("Then you feel a hand slowly squeeze your left shoulder, as you turn to look at the hand")
+    time.sleep(3)
+    print("you see its green and boney with thick hairs and boils covering it, the fingers are long and the nails are rotten and razor sharp")
+    time.sleep(3)
+    print("You try to run but the hand picks you up crushing the bones in your shoulder, now suspended in mid air, you start screaming for help .... 'HELP! HELP!'")
+    time.sleep(3)
+    print("As you struggle, more hands fire out of the darkness grabbing all four of your limbs. You struggle with no success") 
+    time.sleep(3)
+    print("The breathing you heard behind you stops and there is a whisper 'This is really going to hurt!' followed by a deep, echoing, demonic laughter 'HAHAHAHAHA!!'")
+    time.sleep(3)
+    print("Suddenly your limbs get ripped from your body. As your torso falls to the floor with a deafing thud. Blood leaking from the mouth and eyes, leaving a pool on the floor. Your body then gets dragged into the darkness in the blink of an eye where giggling can be heard and the consumption of flesh. ")
+    time.sleep(3)
+    print("Sorry my friend but you died so GAME OVER!")
+    time.sleep(10)
+    exit()
+
+time.sleep(3)
+print("As you stop at the end of the corridor, trying to catch your breath and looking for a way out.")
+time.sleep(3)
+print("you see the fire exit door, you attempt to open it but its locked. You throw your whole body against it hoping it would open but it didn't move an inch.")
+time.sleep(3)
+print("All growling and snarling you heard earlier has stopped, the corridor is dead silent, you suddenly hear machines winding up then the emergency lights come on. it fills the corridor with a dim misty red hue.")
+time.sleep(3)
+print("You look down the corridor and see a blurry figure in the distance, in your panic as you continue to look for a way out, The figure seems to be getting slowly closer and closer. As you bang on the fire exit door and the walls looking for a way out, the bone chilling sounds get louder.")
+time.sleep(3)
+print("You hear a loud creaking noice, as you spin around, you see that the fire exit is wide open. You look over your shoulder at the figure to see it is only a few feet away. Its hand stretched out towards you and its fingers spread as it tries to grab you.")
+time.sleep(3)
+
+        
+
+
+
+
+
+
+
+
+
+
+
